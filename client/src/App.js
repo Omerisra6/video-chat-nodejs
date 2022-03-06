c7de2f8432cc656822ee67936f04a18aaac88eca
omer <omer.israeli@boyar.org.il>
2022-03-06 18:47:22 +0200

    New: Socket provider added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import Chat from "./components/Chat";
 import StartChat from "./components/StartChat";
+import {SocketContext, socket} from './context/socket';
 
 function App() {
 
   const [ user, setUser ] = useState( '' )
 
   return (
-    <div className="App">
-        { user ? <Chat user={user} setUser={setUser}/> :  <StartChat setUser={setUser}/> }
-    </div>
+    <SocketContext.Provider value={socket}>
+      { user ? <Chat user={user} setUser={setUser}/> :  <StartChat setUser={setUser}/> }
+    </SocketContext.Provider>
   );
 }
 
---
+++ client/src/context/socket.js
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import { io } from "socket.io-client";
+
+export const socket = io( process.env.REACT_APP_SOCKET_URL );
+export const SocketContext = React.createContext( null );
+
+export const useSocket = () => {
+    return useContext( SocketContext )  
+}
