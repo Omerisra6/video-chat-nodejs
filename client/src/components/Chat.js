6e773d5741ae64345c68db926d0326a711b8e224
omer <omer.israeli@boyar.org.il>
2022-03-13 17:08:20 +0200

    New: Get user name by id added

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,23 +1,51 @@
 import React, { useState } from 'react'
+import { useSocket } from '../context/socket'
 import useOnChatEvents from '../hooks/useOnChatEvents'
 import useOnPeerEvents from '../hooks/useOnPeerEvents'
+import StyledStreamsContainer from './ui/styled/StyledStreamsContainer'
 import Video from './ui/Video'
 
-export default function Chat( ) {
+export default function Chat( { user, setUser } ) {
 
   const [ members, setMembers ] = useState( [] )
   const [ streams, setStreams ] = useState( [] )
+  const currentId = useSocket().id
   useOnChatEvents( setMembers )
-  useOnPeerEvents( setStreams, streams )
-  console.log( streams )
-
+  useOnPeerEvents( setStreams )
 
   return (
-    <div>
+
+    <StyledStreamsContainer>
+      
       { streams.map( stream => {
 
-        return <Video stream={stream}/>
+        const name = getNameById( stream[ 'id' ], currentId )
+        return <Video stream={stream} name={ name }/>
+
       })}
-    </div>
+
+    </StyledStreamsContainer> 
+    
   )
+
+  //Finds user in members array by id
+  function getNameById ( id, currentId ) {
+
+    if ( currentId === id) {
+      return user
+    }
+
+    let name
+    members.every( member => {
+
+      if ( Object.keys( member )[ 0 ] === id ) {
+        name =  Object.values( member )[ 0 ]
+        return false
+      }
+      
+      return true
+    });
+
+    return name
+  }
 }
