d632e6ed6f7bedcd2e0295badc2c262f8a31f87d
omer <omer.israeli@boyar.org.il>
2022-03-28 20:36:26 +0300

    New: Heroku adjustments

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { io } from "socket.io-client";
 
-const socket = io( process.env.REACT_APP_SOCKET_URL );
+const socket = io( process.env.REACT_APP_SOCKET_URL || window.location.host );
 export const SocketContext = React.createContext( null );
 
 export const useSocket = () => {
